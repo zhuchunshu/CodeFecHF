f98481de491309ba35c1b56b3e173f9a093e51ce
zhuchunshu <laravel@88.com>
2021-06-15 14:55:30 +0800

    更新: 接入Laravel Mix

---
+++ resources/mix/webpack.mix.js
@@ -0,0 +1,16 @@
+/**
+ * @author Inkedus
+ * @licence MIT
+ * @link https://github.com/zhuchunshu/CoedFecHF
+ * @type {mix.Api}
+ */
+let mix = require('laravel-mix');
+
+function public_path(path=""){
+    return "../../public"+path;
+}
+
+mix.js('../js/app.js', public_path("/js"));
+
+
+mix.setPublicPath(public_path())
