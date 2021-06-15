ca32df5ac872ed5b8e1e31d5655b331b2dadafae
zhuchunshu <laravel@88.com>
2021-06-15 14:00:43 +0800

    更新:新增辅助函数文件

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * CodeFec - Hyperf
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://github.com/zhuchunshu
+ * @document https://codefec.com
+ * @contact  laravel@88.com
+ * @license  https://github.com/zhuchunshu/CodeFecHF/blob/master/LICENSE
  */
 namespace App\Controller;
 
@@ -15,11 +15,11 @@ use Hyperf\View\RenderInterface;
 
 class IndexController extends AbstractController
 {
-    public function index(RenderInterface $render): \Psr\Http\Message\ResponseInterface
+    public function index(RenderInterface $render): string
     {
 //        $user = $this->request->input('user', 'Hyperf');
 //        $method = $this->request->getMethod();
 
-        return $render->render('index');
+        return test();
     }
 }
