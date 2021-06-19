8daa020e80bc7d410c6dc24ab4b8c7a280b3f41a
zhuchunshu <laravel@88.com>
2021-06-19 14:32:05 +0800

    更新:新增辅助函数

--- app/helpers.php
+++ app/helpers.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Hyperf\View\RenderInterface;
 use App\CodeFec\Menu\MenuInterface;
+use Hyperf\Utils\ApplicationContext;
 use Hyperf\Contract\SessionInterface;
 
 function public_path($path = ''): string
@@ -47,6 +48,93 @@ if (!function_exists("arr_has")) {
     function arr_has($array, $keys)
     {
         return Arr::has($array, $keys);
+    }
+}
+
+/**
+ * 容器实例
+ */
+if (!function_exists('container')) {
+    function container()
+    {
+        return ApplicationContext::getContainer();
+    }
+}
+
+/**
+ * redis 客户端实例
+ */
+if (!function_exists('redis')) {
+    function redis()
+    {
+        return container()->get(Redis::class);
+    }
+}
+
+/**
+ * server 实例 基于 swoole server
+ */
+if (!function_exists('server')) {
+    function server()
+    {
+        return container()->get(ServerFactory::class)->getServer()->getServer();
+    }
+}
+
+/**
+ * websocket frame 实例
+ */
+if (!function_exists('frame')) {
+    function frame()
+    {
+        return container()->get(Frame::class);
+    }
+}
+
+/**
+ * websocket 实例
+ */
+if (!function_exists('websocket')) {
+    function websocket()
+    {
+        return container()->get(WebSocketServer::class);
+    }
+}
+
+/**
+ * 缓存实例 简单的缓存
+ */
+if (!function_exists('cache')) {
+    function cache()
+    {
+        return container()->get(Psr\SimpleCache\CacheInterface::class);
+    }
+}
+
+/**
+ * 控制台日志
+ */
+if (!function_exists('stdLog')) {
+    function stdLog()
+    {
+        return container()->get(StdoutLoggerInterface::class);
+    }
+}
+
+/**
+ * 文件日志
+ */
+if (!function_exists('logger')) {
+    function logger()
+    {
+        return container()->get(LoggerFactory::class)->make();
+    }
+}
+
+if (!function_exists('response')) {
+    function response()
+    {
+        return container()->get(ResponseInterface::class);
     }
 }
 
